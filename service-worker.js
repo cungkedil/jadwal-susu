60fae773bc77b9cdc02ab12859a4066a93a34c78
fauzanalfaruqi <fauzanalfaruqi@gmail.com>
2025-05-05 23:22:48 +0700

    update service worker

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,31 @@
-// service-worker.js
-self.addEventListener("install", (event) => {
+const CACHE_NAME = 'v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './title.css',
+  './main.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open("v1").then((cache) => {
-      return cache.addAll([
-        '/jadwal-susu/',
-        '/jadwal-susu/index.html',
-        '/jadwal-susu/style.css',
-        '/jadwal-susu/title.css',
-        '/jadwal-susu/main.js',
-        '/jadwal-susu/manifest.json',
-        '/jadwal-susu/icons/icon-192.png',
-        '/jadwal-susu/icons/icon-512.png'
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('activate', event => {
+  // clean up old caches here
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(cached => cached || fetch(event.request))
   );
 });
